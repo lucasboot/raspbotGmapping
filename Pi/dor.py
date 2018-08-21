6982385cc15c9752a599c8b84b4bc63d3b39edf3
lucasboot <varela.alves@outlook.com>
2018-08-21 19:28:45 +0100

    contador de giros em dor.py

--- Pi/dor.py
+++ Pi/dor.py
@@ -4,7 +4,7 @@ import time
 import math
 import rospy
 from std_msgs.msg import Int16
-
+rospy.init_node('calcvelesquerda', anonymous=True)
 dist_meas = 0.00
 d_m =0
 sensor = 20
@@ -17,18 +17,20 @@ def cb(data):
 
 def cb2(data):
 	global contador
-	contador = data.data
+	global pulso
+	if(pulso != data.data):
+		contador = contador + 1
 
 def calculate_speed(r_cm, pulsos):
 	global dist_meas,m_per_sec, sensor, d_m
 	if pulsos !=0:							# to avoid DivisionByZero error
-		circ_cm = (2*math.pi)*r_cm			# calculate wheel circumference in CM
-		d_m = circ_cm/100 			# convert cm to m
-		m_per_sec = d_m/5		# calculate m/sec
+		circ_cm = float((2*math.pi)*r_cm)			# calculate wheel circumference in CM
+		d_m =float( circ_cm/100.0) 			# convert cm to m
+		m_per_sec =float(d_m/3.0)		# calculate m/sec
 		dist_meas = (pulsos/sensor) * circ_cm	# measure distance traverse in cm
 		return m_per_sec
 	else:
-		return 0
+		return 0.0
 
 
 if __name__ == '__main__':
@@ -36,9 +38,11 @@ if __name__ == '__main__':
 		start = time.time()
 		global contador
 		global pulso
-		sub2 = rospy.Subscriber('lwheel', Int16, cb2)
-		while time.time() < start +5:
-			sub = rospy.Subscriber('lwheel', Int16, cb)
-		giros = pulso - contador
-		calculate_speed(3.225, giros)	# call this function with wheel radius as parameter
-		sleep(0.1)
+		sub2 = rospy.Subscriber('lwheel', Int16, cb)
+		contador = pulso
+		while time.time() < start +3:
+			sub = rospy.Subscriber('lwheel', Int16, cb2)
+		giros = contador - pulso
+		print(giros)
+		#print(calculate_speed(3.225, giros))	# call this function with wheel radius as parameter
+		time.sleep(0.01)
