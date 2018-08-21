b4f360c4ae62f4ecd3cd3881eca2f470056833e7
lucasboot <varela.alves@outlook.com>
2018-08-21 20:03:08 +0100

    testeenco7

--- Pi/testeencoders.py
+++ Pi/testeencoders.py
@@ -52,17 +52,19 @@ cont2 = 0
 allStop()
 try:
 	while (True):
-    		forwardDrive()
+    	#	forwardDrive()
 		if(gpio.input(20) == 1):
         		cont1 = cont1 + 1
         		msg1.data = cont1
         		pub1.publish(msg1)
         		print cont1
+			sleep(1)
 		if (gpio.input(21) == 1):
         		cont2 = cont2 + 1
         		msg2.data = cont2
         		pub2.publish(msg2)
         		print cont2
+			sleep(1)
 	
 except rospy.ROSInterruptException:	
 		allStop()
