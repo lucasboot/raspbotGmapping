f4033c097d1fcfb33ae55ac1559e4b257acc7cdc
lucasboot <varela.alves@outlook.com>
2018-08-21 20:42:05 +0100

    dados para Ori

--- Pi/pulsos.py
+++ Pi/pulsos.py
@@ -7,44 +7,41 @@ import numpy
 # Motor A, Left Side GPIO CONSTANTS
 PWM_FORWARD_LEFT_PIN = 26	# GPIO26- Pra frente
 PWM_REVERSE_LEFT_PIN = 19	# GPIO19 - Pra tras
-# Motor B, Right Side GPIO CONSTANTS
-PWM_FORWARD_RIGHT_PIN = 6	# GPIO06 
-PWM_REVERSE_RIGHT_PIN = 13	# GPIO13 
 
 
-encoder1 = DigitalInputDevice(21)
+encoder1 = DigitalInputDevice(20)
 forwardLeft = PWMOutputDevice(PWM_FORWARD_LEFT_PIN, True, 0, 1000)
 reverseLeft = PWMOutputDevice(PWM_REVERSE_LEFT_PIN, True, 0, 1000)
-
-forwardRight = PWMOutputDevice(PWM_FORWARD_RIGHT_PIN, True, 0, 1000)
-reverseRight = PWMOutputDevice(PWM_REVERSE_RIGHT_PIN, True, 0, 1000)
 
 def allStop():
 	print("Parando")
 	forwardLeft.value = 0
 	reverseLeft.value = 0
-	forwardRight.value = 0
-	reverseRight.value = 0
 
-def forwardDrive(valor:
+def forwardDrive(valor):
 	print("Para frente")
-	forwardLeft.value = 0.0
-	reverseLeft.value = valor
-	forwardRight.value = 0.0
-	reverseRight.value = valor
+	forwardLeft.value = valor
+	reverseLeft.value =  0.0
 
 def main():
+    arq = open("dados.txt", "w")
     cont = 0
     vet = []
-    start = time.time()
-    for i in numpy.arange(0.5,1, 0.05):
+    for i in numpy.arange(0.5,1, 0.04):
+	start = time.time()
         forwardDrive(i)
         while time.time() < start +4:
                 encoder1.wait_for_active()
                 cont = cont +1
                 encoder1.wait_for_inactive()
-        vet.append(cont1)
+	vet.append(i)
+        vet.append(cont)
+	arq.write(str(i))
+	arq.write(", ")
+	arq.write(str(cont))
+	arq.write("\n")
         cont = 0
+    arq.close()
     for v in vet:
         print(v)
 
