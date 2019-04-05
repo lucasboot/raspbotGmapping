ca3ae6968946f8567d7ca761621f23b250b8dcbd
Lucas Alves <varela.alves@outlook.com>
2019-04-05 17:49:31 -0300

    integracao do US +ROS

--- rangesensor.py
+++ rangesensor.py
@@ -5,15 +5,15 @@ import rospy
 from std_msgs.msg import Float64
 
 GPIO.setmode(GPIO.BCM) 
-GPIO_TRIGGER = 23
+GPIO_TRIGGER = 23 
 GPIO_ECHO = 24
 GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
 GPIO.setup(GPIO_ECHO, GPIO.IN)
 
 def distance():
+	#metodo para calculo da distancia >
 	GPIO.output(GPIO_TRIGGER, True)
-# set Trigger after 0.01ms to LOW
-	time.sleep(0.00001)
+	time.sleep(0.00001) #set trigger apos 0,01ms
 	GPIO.output(GPIO_TRIGGER, False)
 	StartTime = time.time()
 	StopTime = time.time()
@@ -23,20 +23,18 @@ def distance():
 		StopTime = time.time()
 
 	TimeElapsed = StopTime - StartTime
-	distance = (TimeElapsed*34300)/2
+	distance = (TimeElapsed*34300)/2 #formula para obtencao da distancia
 
 	return distance
 
 if __name__ == '__main__':
-	pub = rospy.Publisher('distancia', Float64, queue_size=1)
-	rospy.init_node('rangesensor', anonymous=True)
-	rate = rospy.Rate(1)
+	pub = rospy.Publisher('distancia', Float64, queue_size=1) #criacao de um publicador no topico distancia
+	rospy.init_node('rangesensor', anonymous=True) #inicializacao do node rangesensor
+	rate = rospy.Rate(1) #rate para execucao dos comandos do ROS
 	try:
 		while True:
-			dist = distance()
-			pub.publish(dist)
-#print ("distancia = %.1f cm" % dist)
-
+			dist = distance() #le o valor do ultrassom e coloca o valor na variavel dist
+			pub.publish(dist) #publica a distancia no topico distancia
 	except rospy.ROSInterruptException:
 		print("finalizado")
 		GPIO.cleanup()
