6a430e6a4e6180223bffb88b495e470ffc968ad5
Lucas Alves <varela.alves@outlook.com>
2019-04-16 16:11:29 -0300

    laserScan infos

--- microlidar/microlidar.ino
+++ microlidar/microlidar.ino
@@ -1,31 +1,51 @@
 #include "Adafruit_VL53L0X.h"
 #include <ros.h>
 #include <std_msgs/Float64.h>
-
+#include <sensor_msgs/LaserScan.h>
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
 
 ros::NodeHandle  lidar; //inicializaco do node do microlidar
-
-std_msgs::Float64 strr_msg; //declaracao do tipo da variavel de distancia
-ros::Publisher microlidar("distancia", &strr_msg); //criacao do publisher, com o tipo da variavel criada referenciado
+sensor_msgs::LaserScan x;
+ros::Publisher scan_pub ("scan", &x);
+float dist;
 
 void setup() {
-  Serial.begin(9600); //rate de comunicacao do arduino
   lidar.initNode(); //start do node
-  lidar.advertise(microlidar); //definicao do publisher do node
+  Serial.begin(9600); //rate de comunicacao do arduino
+  lidar.advertise(scan_pub); 
+
 }
 
 
 void loop() {
-  VL53L0X_RangingMeasurementData_t measure;
-  lox.rangingTest(&measure, false); 
-  if (measure.RangeStatus != 4) {  
-    strr_msg.data = measure.RangeMilliMeter;
-    microlidar.publish(&strr_msg); //publicando o valor de range lido no topico /distancia
+  unsigned int num_readings = 50;
+  double laser_frequency = 40;
+  double ranges[num_readings];
+  double intensities[num_readings];
+  //ros::Rate r(1.0);
+  ros::Time scan_time = ros::Time::now();
+  sensor_msgs::LaserScan scan;
+  scan.header.stamp = scan_time;
+  scan.header.frame_id = "laser_frame";
+  scan.angle_min = -1.57;
+  scan.angle_max = 1.57;
+  scan.angle_increment = 3.14/num_readings;
+  scan.time_increment = (1/laser_frequency) / (num_readings);
+  scan.range_min = 0.05;
+  scan.range_max = 3.8;
+
+  for (unsigned int i = 0; i< num_readings; i++){
+    VL53L0X_RangingMeasurementData_t measure;
+    lox.rangingTest(&measure, false); 
+    if (measure.RangeStatus != 4) {  
+      dist = measure.RangeMilliMeter;
+      scan.ranges[i] = dist;  //publicando o valor de range lido no topico /distancia
+      scan.intensities[i] = 1;
+    }
     
   }
+  scan_pub.publish(&scan);
   lidar.spinOnce(); //comando para o ROS compreender que houve uma comunicacao entre topicos
-  delay(100);
 }
