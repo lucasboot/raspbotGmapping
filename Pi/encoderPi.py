e791a3bdb634442de21812bd2226eb08060523dc
Lucas Alves <varela.alves@outlook.com>
2019-05-24 13:24:46 -0300

    Encoders funcionando com o rasp

--- Pi/encoderPi.py
+++ Pi/encoderPi.py
@@ -1,33 +1,34 @@
-#!/usr/bin/env python
-import RPIO
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+import RPi.GPIO as gpio
+import time
 import rospy
 from std_msgs.msg import Int16
 
-#definir a funcao de callback
-
 rospy.init_node('encoders', anonymous=True)
+gpio.setmode(gpio.BCM)
+gpio.setup(20, gpio.IN, pull_up_down = gpio.PUD_DOWN)
+gpio.setup(21, gpio.IN, pull_up_down = gpio.PUD_DOWN)
+gpio.setmode(gpio.BCM)  
 pub1 = rospy.Publisher('lwheel', Int16, queue_size=1)
 pub2 = rospy.Publisher('rwheel', Int16, queue_size=1)
 msg1 = Int16()
 msg2 = Int16()
 cont1 = 0
 cont2 = 0
-def gpio_cb(gpio_id, value):
-   cont1 = cont +1
-   ms1.data = msg1
-   pub1.publish(ms1)
 
-def gpio_cb2(gpio_id, value):
-   cont2 = cont2 +1
-   ms2.data = msg2
-   pub2.publish(ms2)
-#adicionar a interrupcao
-try:
-    while True:
-        RPIO.add_interrupt_callback(20, gpio_cb1, edge='falling', pull_up_down=RPIO.PUD_UP, threaded_callback=True, debounce_timeout_ms=0)
-        RPIO.add_interrupt_callback(21, gpio_cb2, edge='falling', pull_up_down=RPIO.PUD_UP, threaded_callback=True, debounce_timeout_ms=0)
-    #colocar a interrupcao em loop
-        RPIO.wait_for_interrupts()
-except rospy.ROSInterruptException:
-		GPIO.cleanup()
-		pass
+while True:
+	if(gpio.input(20) == 1):
+        cont1 = cont1 + 1
+        msg1.data = cont1
+        pub1.publish(msg1)
+        print cont1
+    if (gpio.input(21) == 1):
+        cont2 = cont2 + 1
+        msg2.data = cont2
+        pub2.publish(msg2)
+        print cont2
+	time.sleep(0.1)
+gpio.cleanup()
+exit()
