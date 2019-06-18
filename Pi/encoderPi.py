6f834d27c0800302531d8a0263a396712ec0b376
lucasboot <varela.alves@outlook.com>
2019-06-18 16:31:23 +0000

    novo algoritmo para a interrupcao

--- Pi/encoderPi.py
+++ Pi/encoderPi.py
@@ -3,32 +3,31 @@
 
 import RPi.GPIO as gpio
 import time
-import rospy
-from std_msgs.msg import Int16
 
 rospy.init_node('encoders', anonymous=True)
 gpio.setmode(gpio.BCM)
 gpio.setup(20, gpio.IN, pull_up_down = gpio.PUD_DOWN)
 gpio.setup(21, gpio.IN, pull_up_down = gpio.PUD_DOWN)
 gpio.setmode(gpio.BCM)  
-pub1 = rospy.Publisher('lwheel', Int16, queue_size=1)
-pub2 = rospy.Publisher('rwheel', Int16, queue_size=1)
-msg1 = Int16()
-msg2 = Int16()
 cont1 = 0
 cont2 = 0
 
-while True:
-	if(gpio.input(20) == 1):
-        	cont1 = cont1 + 1
-        	msg1.data = cont1
-        	pub1.publish(msg1)
-        	print cont1
-	if (gpio.input(21) == 1):
-        	cont2 = cont2 + 1
-        	msg2.data = cont2
-        	pub2.publish(msg2)
-        	print cont2
-	time.sleep(0.1)
-gpio.cleanup()
-exit()
+def eventoesquerdo(channel):
+	global cont1 = cont1 + 1
+	print(cont1)
+
+def eventodireito(channel):
+	global cont2 = cont2 + 1
+        print(cont2)
+
+
+gpio.add_event_detect(20, gpio.RISING, callback=eventoesquerdo, bouncetime=300)
+gpio.add_event_detect(21, gpio.RISING, callback=eventodireito, bouncetime=300)
+
+try:
+
+
+except KeyboardInterrupt:  
+    gpio.cleanup()        
+gpio.cleanup()    	
+
