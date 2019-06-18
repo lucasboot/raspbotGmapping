7f90ea93d0c3f52aa1c2b1cea2d270be3598b379
lucasboot <varela.alves@outlook.com>
2019-06-18 18:14:05 +0000

    rosificado

--- Pi/encoderesquerdo.py
+++ Pi/encoderesquerdo.py
@@ -1,16 +1,18 @@
 from gpiozero import DigitalInputDevice
+import rospy
+from std_msgs.msg import Int16
 
+rospy.init_node('encoderEsquerdo', anonymous=True)
+msg = Int16()
+pub = rospy.Publisher('lwheel', Int16, queue_size=1)
 encoder1 = DigitalInputDevice(20)
 cont1 = 0
-def contando():
-        global cont1
-        cont1 = cont1 +1
-        print cont1
-
 while True:
         encoder1.wait_for_active()
         global cont1
         cont1 = cont1 +1
+        msg.data = cont1
+        pub.publish(msg)
         print cont1
         encoder1.wait_for_inactive()
 
