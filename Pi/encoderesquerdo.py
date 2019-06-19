0b6f6ca7fb57f61016e6b4375dfb7ed5d3b5f94d
lucasboot <varela.alves@outlook.com>
2019-06-19 19:33:10 +0000

    aa

--- Pi/encoderesquerdo.py
+++ Pi/encoderesquerdo.py
@@ -1,21 +1,26 @@
 from gpiozero import DigitalInputDevice
 import rospy
 from std_msgs.msg import Int16
+import time
 
 rospy.init_node('encoderEsquerdo', anonymous=True)
 msg = Int16()
+giros = Int16()
+giros.data = 0
 pub = rospy.Publisher('lwheel', Int16, queue_size=1)
-encoder1 = DigitalInputDevice(20)
+pub2 = rospy.Publisher('giros', Int16, queue_size=1)
+encoder1 = DigitalInputDevice(21)
 cont1 = 0
 while True:
-        encoder1.wait_for_active()
-        global cont1
-        cont1 = cont1 +1
-        msg.data = cont1
-        pub.publish(msg)
-        print cont1
-        encoder1.wait_for_inactive()
-
-
-
-
+        global giros = cont1
+        start = time.time()
+        while time.time() < start +5:
+                encoder1.wait_for_active()
+                global cont1
+                cont1 = cont1 +1
+                msg.data = cont1
+                pub.publish(msg)
+                print cont1
+                encoder1.wait_for_inactive()
+        giros.data = cont1 - giros.data
+        pub2.publish(giros)
