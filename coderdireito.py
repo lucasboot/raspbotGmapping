533ff1fa41c64cbc4377e6e517ad6e3eee4be84d
lucasboot <varela.alves@outlook.com>
2019-06-28 20:25:46 +0000

    novas coisas

---
+++ coderdireito.py
@@ -0,0 +1,35 @@
+from gpiozero import DigitalInputDevice
+import rospy
+from std_msgs.msg import Int16
+from std_msgs.msg import Float32
+import time
+
+rospy.init_node('encoderDireito', anonymous=True)
+msg = Int16()
+pub3 = rospy.Publisher('rwheel', Int16, queue_size=1)
+pub4 = rospy.Publisher('girosr', Float32, queue_size=1)
+encoder1 = DigitalInputDevice(20)
+cont1 = 0
+while True:
+	inicio  = cont1
+        start = time.time()
+        giros = Float32()
+        while time.time() < start +1:
+		news = time.time()
+		while (encoder1.value == 0):
+			pub3.publish(msg)
+                        if((news + 1 )< time.time()):
+                                giros.data = 0.0
+                                pub4.publish(giros)
+                global cont1
+                cont1 = cont1 +1
+                msg.data = cont1
+                pub3.publish(msg)
+		news=time.time()
+                while(encoder1.value == 1):
+			pub3.publish(msg)
+                        if((news + 1 )< time.time()):
+                                giros.data = 0.0
+                                pub4.publish(giros)
+        giros.data =float((cont1 - inicio))/20.0
+pub4.publish(giros)
---
+++ coderesq.py
@@ -0,0 +1,36 @@
+from gpiozero import DigitalInputDevice
+import rospy
+from std_msgs.msg import Int16
+from std_msgs.msg import Float32
+import time
+
+rospy.init_node('encoderEsquerdo', anonymous=True)
+msg = Int16()
+pub = rospy.Publisher('lwheel', Int16, queue_size=1)
+pub2 = rospy.Publisher('girosl', Float32, queue_size=1)
+encoder1 = DigitalInputDevice(21)
+cont1 = 0
+while True:
+	inicio  = cont1
+        start = time.time()
+        giros = Float32()
+        while time.time() < start +1:
+		while (encoder1.value == 0):
+                        news = time.time()
+			pub.publish(msg)
+                       # if((news + 3)< time.time()):
+                        #        giros.data = 0.0
+                         #       pub2.publish(giros)
+                global cont1
+                cont1 = cont1 +1
+                msg.data = cont1
+                pub.publish(msg)
+                while(encoder1.value == 1):
+                        news = time.time()
+			pub.publish(msg)
+
+                       #if((news + 3 )< time.time()):
+                        #        giros.data = 0.0
+                         #       pub2.publish(giros) 
+        giros.data =float(cont1 - inicio)/20.0
+pub2.publish(giros)
