429dccf4858591b202997921ef6078b109c5b676
lucasboot <varela.alves@outlook.com>
2019-07-23 12:20:24 -0300

    Identacao ok

--- Pi/encoderdireitoTRAS.py
+++ Pi/encoderdireitoTRAS.py
@@ -42,30 +42,28 @@ cont1 = 0
 def parafrente():
 	forwardDrive()
 	inicio  = cont1
-        start = time.time()
-        giros = Float32()
-        while time.time() < start +1:
-	    news = time.time()
-	    while (encoder1.value == 0):
+    start = time.time()
+    giros = Float32()
+    while time.time() < start +1:
+		news = time.time()
+	while (encoder1.value == 0):
 		pub.publish(msg)
-               	if((news + 1)< time.time()):
-                        giros.data = 0.0
-                        pub2.publish(giros)
-            global cont1
-            cont1 = cont1 +1
-            msg.data = cont1
-            pub.publish(msg)
-	    news = time.time()
-            while(encoder1.value == 1):
+            if((news + 1)< time.time()):
+                giros.data = 0.0
+                pub2.publish(giros)
+    global cont1
+    cont1 = cont1 +1
+	msg.data = cont1
+	pub.publish(msg)
+	news = time.time()
+    while(encoder1.value == 1):
 		pub.publish(msg)
-
 	    if((news + 1)< time.time()):
                 giros.data = 0.0
                 pub2.publish(giros) 
-            giros.data =float(cont1 - inicio)/20.0
-	    pub2.publish(giros)
-
-        pub2.publish(giros)
+    giros.data =float(cont1 - inicio)/20.0
+	pub2.publish(giros)
+	pub2.publish(giros)
 
 def main():
     parafrente()
--- Pi/encoderesquerdoTRAS.py
+++ Pi/encoderesquerdoTRAS.py
@@ -66,6 +66,35 @@ encoder1 = DigitalInputDevice(20)
 cont1 = 0
 def parafrente(data):
 	if(data):
+<<<<<<< HEAD
+		forwardDrive()
+	else:
+		reverseDrive()
+	inicio  = cont1
+	start = time.time()
+	giros = Float32()
+	while time.time() < start +1:
+		news = time.time()
+		while (encoder1.value == 0):
+			pub.publish(msg)
+			if((news + 1)< time.time()):
+				giros.data = 0.0
+				pub2.publish(giros)
+		global cont1
+		if(data):
+			cont1 = cont1 +1
+		else:
+			cont1 = cont1 -1
+		msg.data = cont1
+		pub.publish(msg)
+		news = time.time()
+		while(encoder1.value == 1):
+			pub.publish(msg)
+			if((news + 1)< time.time()):
+				giros.data = 0.0
+				pub2.publish(giros) 
+		giros.data =float(cont1 - inicio)/20.0
+=======
 			forwardDrive()
 	else:
 			reverseDrive()
@@ -93,6 +122,7 @@ def parafrente(data):
 						giros.data = 0.0
 						pub2.publish(giros) 
 				giros.data =float(cont1 - inicio)/20.0
+>>>>>>> 49abc3dbbabe126d7b5a017c7666d9a8f34f1f9c
 	pub2.publish(giros)
 '''
 def paratras():
