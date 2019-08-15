22cad9ee2bff55278a3530b4c7d47309253d14bf
lucasboot <varela.alves@outlook.com>
2019-08-15 11:55:30 -0300

    sem servo no laser

--- Pi/LaserScan.py
+++ Pi/LaserScan.py
@@ -44,21 +44,23 @@ if __name__ == '__main__':
 				scan.time_increment = (1/laser_frequency)/(num_readings)
 				scan.range_min = 0.05
 				scan.range_max = 10.00
-				value = 3.0
+				#value = 3.0
 				print((scan.angle_max - scan.angle_min)/num_readings)
 				for i in range(0, num_readings, 1):
+					'''
 					value2=(float(value)-10)/10.0 
     					myServo.value=value2
 					value = value + 0.3 # ((scan.angle_max - scan.angle_min)/num_readings)
+					'''
 					scan.ranges.append(tof.get_distance()/1000.0)
 					scan.intensities.append(0)
 					time.sleep(timing/1000000.00)
 				scan_pub.publish(scan)
-				value = 3.0
-				value2=(float(value)-10)/10.0 
-                                myServo.value=value2 
+				#value = 3.0
+				#value2=(float(value)-10)/10.0 
+                                #myServo.value=value2 
 				print(value) 
-				sleep(0.5)
+				sleep(0.000001)
 	except rospy.ROSInterruptException:
 			GPIO.cleanup()
 			pass
